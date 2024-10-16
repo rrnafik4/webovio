40482ff947069823dd0d2954b98e8a5f3ad69a05
ruslannafikov <naf.rus@bk.ru>
2024-10-16 20:43:28 +0500

    first commit

---
+++ src/components/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./index.scss";
+import { Button } from "@mui/material";
+function App() {
+  return (
+    <div className="bg-[red]">
+      <Button variant="contained">ss</Button>
+    </div>
+  );
+}
+
+export default App;
