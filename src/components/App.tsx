999da72c99dc33122c394c494a36a609fdb8b438
ruslannafikov <naf.rus@bk.ru>
2024-10-19 02:20:29 +0500

    header

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,19 @@
 import React from "react";
 import "./index.scss";
 import { Header } from "./header/header";
+import Slider from "./header/jpg/Slider.jpg";
+import { TitleNew } from "./title/title";
 
 function App() {
   return (
     <div className="wrapper">
-      <Header />
+      <div className="container-png _ibg">
+        <img src={Slider} alt="Slider" />
+        <div className="_container">
+          <Header />
+          <TitleNew />
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/title/title.tsx
@@ -0,0 +1,14 @@
+import "./index.scss";
+export const TitleNew = () => {
+  return (
+    <div className="container-title">
+      <div className="text1">A place where</div>
+      <div className="text2 ">A creative agency for redemptive brands</div>
+      <div className="text3">
+        Anteelo is a leading strategic design firm that builds powerful digital
+        solutions for startups and enterprise
+      </div>
+      <div className="text4"> Get in touch</div>
+    </div>
+  );
+};
